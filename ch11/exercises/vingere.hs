58242f6af2f8863c9ca78c08758f5ed0f983c7b5
Kamal <kamal.farahani90@gmail.com>
2024-05-03 21:33:47 +0330

    refactor

--- ch11/exercises/vingere.hs
+++ ch11/exercises/vingere.hs
@@ -8,9 +8,7 @@ encrypt =
     map (shift . codeToShift)
     where
         padding                = ord 'A'
-        codeToShift :: (Char, Char) -> (Char, Int)
         codeToShift (x, code)  = (x, ord code - padding)
-        shift :: (Char, Int) -> Char
         shift (char, shift)    = chr ((((ord char - padding ) + shift ) `mod` 26) + padding)
 
 
@@ -18,14 +16,11 @@ vingere :: String -> String -> String
 vingere text key =
     restoreNonAlpha $ encrypt charAndCode
     where
-        charAndCode         = zip clearedText cycledKey
-        clearedText         = removeNonAlpha (toUpperString text)
-        cycledKey           = cycle (toUpperString key)
-        toUpperString :: String -> String
-        toUpperString = map toUpper
-        removeNonAlpha :: String -> String
-        removeNonAlpha = filter isAlpha
-        restoreNonAlpha :: String -> String
+        cycledKey             = cycle (toUpperString key)
+        charAndCode           = zip clearedText cycledKey
+        clearedText           = removeNonAlpha (toUpperString text)
+        toUpperString         = map toUpper
+        removeNonAlpha        = filter isAlpha
         restoreNonAlpha text' = foldl selector "" text
             where
                 selector :: String -> Char -> String
