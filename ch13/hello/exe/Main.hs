7fa37cbd84b5b0a10bd72750dac5cc064354a5ba
Kamal <kamal.farahani90@gmail.com>
2024-06-16 19:55:00 +0330

    add DogsRule

--- ch13/hello/exe/Main.hs
+++ ch13/hello/exe/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import Hello ( sayHello )
+import DogsRule ( dogs )
 
 
 main :: IO ()
 main = do
   sayHello "Haskell"
+  dogs
---
+++ ch13/hello/src/DogsRule.hs
@@ -0,0 +1,9 @@
+module DogsRule
+    ( dogs )
+    where
+
+
+dogs :: IO ()
+dogs = do
+  putStrLn "Who is the doggo?"
+  putStrLn "You are the doggo!"
