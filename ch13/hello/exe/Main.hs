f384440504230a0aa86367092828a4c657dec5d2
Kamal <kamal.farahani90@gmail.com>
2024-06-16 21:19:36 +0330

    add input message

--- ch13/hello/exe/Main.hs
+++ ch13/hello/exe/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 import Hello ( sayHello )
 import DogsRule ( dogs )
+import System.IO (hSetBuffering, stdout, BufferMode (NoBuffering))
 
 
 main :: IO ()
 main = do
-  sayHello "Haskell"
+  hSetBuffering stdout NoBuffering
+  putStr "Who are you? "
+  name <- getLine
+  sayHello name
   dogs
