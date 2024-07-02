29c1905c767e1657d0648a6c93edde5bcdee5b6d
Kamal <kamal.farahani90@gmail.com>
2024-07-02 13:09:39 +0330

    refactor and format code

--- ch11/exercises/vingere.hs
+++ ch11/exercises/vingere.hs
@@ -1,33 +1,31 @@
 module Vingere where
 
-import Data.Char
-
+import Data.Char (chr, isAlpha, ord, toUpper)
 
 encrypt :: [(Char, Char)] -> String
 encrypt =
-    map (shift . codeToShift)
-    where
-        padding                = ord 'A'
-        codeToShift (x, code)  = (x, ord code - padding)
-        shift (char, shift)    = chr ((((ord char - padding ) + shift ) `mod` 26) + padding)
-
+  map (shift . codeToShift)
+  where
+    padding = ord 'A'
+    codeToShift (x, code) = (x, ord code - padding)
+    shift (char, shift) = chr ((((ord char - padding) + shift) `mod` 26) + padding)
 
 vingere :: String -> String -> String
 vingere text key =
-    restoreNonAlpha $ encrypt charAndCode
-    where
-        cycledKey             = cycle (toUpperString key)
-        charAndCode           = zip clearedText cycledKey
-        clearedText           = removeNonAlpha (toUpperString text)
-        toUpperString         = map toUpper
-        removeNonAlpha        = filter isAlpha
-        restoreNonAlpha text' = foldl selector "" text
-            where
-                selector :: String -> Char -> String
-                selector acc x = 
-                    if not (isAlpha x)
-                        then acc ++ [x]
-                        else acc ++ [text' !! (length acc - getNonAlpha acc)]
-                    where
-                        getNonAlpha :: String -> Int
-                        getNonAlpha = length . filter (not . isAlpha)
+  restoreNonAlpha $ encrypt charAndCode
+  where
+    cycledKey = cycle (toUpperString key)
+    charAndCode = zip clearedText cycledKey
+    clearedText = removeNonAlpha (toUpperString text)
+    toUpperString = map toUpper
+    removeNonAlpha = filter isAlpha
+    restoreNonAlpha text' = foldl selector "" text
+      where
+        selector :: String -> Char -> String
+        selector acc x =
+          if not (isAlpha x)
+            then acc ++ [x]
+            else acc ++ [text' !! (length acc - getNonAlpha acc)]
+          where
+            getNonAlpha :: String -> Int
+            getNonAlpha = length . filter (not . isAlpha)
