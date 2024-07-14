7a4bec004a062688ac8ea6ccc3107ca41a9bacd0
Kamal <kamal.farahani90@gmail.com>
2024-07-15 01:46:08 +0330

    implement main

--- ch14/morse/app/Main.hs
+++ ch14/morse/app/Main.hs
@@ -1,6 +1,59 @@
 module Main (main) where
 
-import Lib
+import Control.Monad (forever, when)
+import Data.List (intercalate)
+import Data.Traversable (traverse)
+import Morse (morseToChar, stringToMorse)
+import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (hGetLine, hIsEOF, stdin)
+
+convertToMorse :: IO ()
+convertToMorse = forever $ do
+  weAreDone <- hIsEOF stdin
+  when weAreDone exitSuccess
+  line <- hGetLine stdin
+  convertLine line
+  where
+    convertLine :: String -> IO ()
+    convertLine line = do
+      let morse = stringToMorse line
+      case morse of
+        (Just strList) -> do
+          putStrLn (unwords strList)
+        Nothing -> do
+          putStrLn "ERROR: "
+          exitFailure
+
+convertFromMorse :: IO ()
+convertFromMorse = forever $ do
+  weAreDone <- hIsEOF stdin
+  when weAreDone exitSuccess
+  line <- hGetLine stdin
+  convertLine line
+  where
+    convertLine :: String -> IO ()
+    convertLine line = do
+      let decoded :: Maybe String
+          decoded = traverse morseToChar (words line)
+      case decoded of
+        (Just s) -> putStrLn s
+        Nothing -> do
+          putStrLn "ERROR: "
+          exitFailure
 
 main :: IO ()
-main = someFunc
+main = do
+  mode <- getArgs
+  case mode of
+    [arg] ->
+      case arg of
+        "from" -> convertFromMorse
+        "to" -> convertToMorse
+        _ -> argError
+    _ -> argError
+  where
+    argError = do
+      putStrLn "ERROR: "
+      putStrLn "Please specify the correct mode: 'from' or 'to'"
+      exitFailure
