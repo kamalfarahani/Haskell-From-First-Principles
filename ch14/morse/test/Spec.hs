b1dcffcbc3ad6334ee16bb08f263b225da95b321
Kamal <kamal.farahani90@gmail.com>
2024-07-15 15:56:35 +0330

    add tests

--- ch14/morse/test/Spec.hs
+++ ch14/morse/test/Spec.hs
@@ -1,2 +1,27 @@
+module Main where
+
+import Data.Map qualified as M
+import Morse
+import Morse (letterToMorse)
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck prop_thereAndBackAgain
+
+allowedChars :: [Char]
+allowedChars = M.keys letterToMorse
+
+allowedMorse :: [MorseCode]
+allowedMorse = M.elems letterToMorse
+
+charGen :: Gen Char
+charGen = elements allowedChars
+
+morseGen :: Gen MorseCode
+morseGen = elements allowedMorse
+
+prop_thereAndBackAgain :: Property
+prop_thereAndBackAgain =
+  forAll
+    charGen
+    (\c -> (charToMorse c >>= morseToChar) == Just c)
