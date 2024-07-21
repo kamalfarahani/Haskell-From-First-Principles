0e1206a184322e36ea2255942b854952c058f1f2
Kamal <kamal.farahani90@gmail.com>
2024-07-21 18:10:51 +0330

    add chapter exercises

---
+++ ch15/exercises/SemigroupInstances.hs
@@ -0,0 +1,121 @@
+module SemigroupInstances where
+
+import Test.QuickCheck
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+newtype Identity a = Identity a
+
+instance (Semigroup a) => Semigroup (Identity a) where
+  (Identity x) <> (Identity y) = Identity (x <> y)
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return (Identity x)
+
+data Two a b = Two a b
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two x y) <> (Two x' y') = Two (x <> x') (y <> y')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return (Two x y)
+
+data Three a b c = Three a b c
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+  (Three x y z) <> (Three x' y' z') = Three (x <> x') (y <> y') (z <> z')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return (Three x y z)
+
+data Four a b c d = Four a b c d
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
+  (Four x y z t) <> (Four x' y' z' t') = Four (x <> x') (y <> y') (z <> z') (t <> t')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    t <- arbitrary
+    return (Four x y z t)
+
+newtype BoolConj = BoolConj Bool
+  deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+  (BoolConj True) <> (BoolConj True) = BoolConj True
+  _ <> _ = BoolConj False
+
+instance Arbitrary BoolConj where
+  arbitrary = do
+    x <- arbitrary
+    return (BoolConj x)
+
+newtype BoolDisj = BoolDisj Bool
+  deriving (Eq, Show)
+
+instance Semigroup BoolDisj where
+  (BoolDisj False) <> (BoolDisj False) = BoolDisj False
+  _ <> _ = BoolDisj True
+
+instance Arbitrary BoolDisj where
+  arbitrary = do
+    x <- arbitrary
+    return (BoolDisj x)
+
+data Or a b
+  = Fst a
+  | Snd b
+
+instance Semigroup (Or a b) where
+  (Snd x) <> _ = Snd x
+  (Fst _) <> (Snd x) = Snd x
+  (Fst _) <> (Fst x) = Fst x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    elements [Fst x, Snd y]
+
+newtype Combine a b = Combine {unCombine :: a -> b}
+
+instance (Semigroup b) => Semigroup (Combine a b) where
+  f <> g = Combine (\x -> unCombine f x <> unCombine g x)
+
+instance (Arbitrary a, Arbitrary b, CoArbitrary a) => Arbitrary (Combine a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return (Combine f)
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivAssoc)
+
+get :: IO ()
+get = do
+  f <- arbitrary
+  print (f 2)
