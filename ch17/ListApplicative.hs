39f91c117015b89b765d8cdf3592037d822812f7
Kamal <kamal.farahani90@gmail.com>
2024-09-18 18:30:22 +0330

    add property testing

--- ch17/ListApplicative.hs
+++ ch17/ListApplicative.hs
@@ -1,22 +1,18 @@
 module ListApplicative where
 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 data List a =
     Nil
   | Cons a (List a)
   deriving (Eq, Show)
 
-
-append :: List a -> List a -> List a
-append Nil ys = ys
-append xs Nil = xs
-append (Cons x xs) ys = Cons x $ xs `append` ys
-
-
 instance Functor List where
   fmap :: (a -> b) -> List a -> List b
   fmap _ Nil = Nil
   fmap f (Cons a as) = Cons (f a) (fmap f as)
-
 
 instance Applicative List where
   pure :: a -> List a
@@ -26,15 +22,35 @@ instance Applicative List where
   _ <*> Nil = Nil
   fs <*> xs = flatMap (<$> xs) fs
 
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = do
+    hlist <- arbitrary
+    let l = haskellListToList hlist
+    return l
+
+instance (Eq a) => EqProp (List a) where
+  (=-=) = eq
+
+haskellListToList :: [a] -> List a
+haskellListToList = foldr Cons Nil
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append xs Nil = xs
+append (Cons x xs) ys = Cons x $ xs `append` ys
 
 fold :: (a -> b -> b) -> b -> List a -> b
 fold _ b Nil = b
 fold f b (Cons h t) = f h (fold f b t)
 
-
 concat' :: List (List a) -> List a
 concat' = fold append Nil
 
-
 flatMap :: (a -> List b) -> List a -> List b
 flatMap f as = concat' $ fmap f as
+
+main :: IO ()
+main = do
+  let trigger :: List (String, Int, [String])
+      trigger = undefined
+  quickBatch $ applicative trigger
